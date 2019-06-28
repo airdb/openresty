0d796f8d349937b04595003297e7cf2f341a105c
Dean <dean.zhang@shopee.com>
2019-06-28 09:38:15 +0800

    test

--- conf/lua/common/gateway.lua
+++ conf/lua/common/gateway.lua
@@ -30,4 +30,23 @@ function _M.set_current_peer(self, msg)
 	-- return ngx.exit(ngx.HTTP_BAD_GATEWAY)
 end
 
+function _M.set_current_peer1(self, msg)
+	node = {
+		host = '127.0.0.1',
+		port = 8111,
+	}
+
+	ngx.log(ngx.INFO, 'set_current_peer_success|host=', node.host, ',port=', node.port, msg)
+	-- local ok, err = balancer.set_current_peer(node.host, node.port)
+	local ok, err = balancer.set_current_peer("127.0.0.1", 8111)
+    if not ok then
+        ngx.log(ngx.ERR, "failed to set the current peer: ", err)
+        return ngx.exit(500)
+    else
+	    ngx.log(ngx.INFO, "xxxx")
+    end
+	-- ngx.log(ngx.DEBUG, 'set_current_peer_success|host=', node.host, ',port=', node.port)
+	-- return ngx.exit(ngx.HTTP_BAD_GATEWAY)
+end
+
 return _M
