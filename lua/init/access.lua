18b7ba6e96bda765fd2281e312466a19a15a49a0
YiNo <dean@airdb.com>
2019-06-30 15:44:57 +0800

    update

--- lua/init/access.lua
+++ lua/init/access.lua
@@ -3,3 +3,23 @@ ngx.header.Server = 'AGW'
 local uustr = require("lua.common.uustr")
 ngx.header['x-airdb-request-id'] = uustr.randomStr(64)
 -- X-Served-By:
+--
+ngx.say(ngx.var.REMOTE_ADDR)
+
+local city = require("lua.ipdb.city")
+-- ipdb = city:new("/usr/local/openresty/nginx/lua/ipdb/mydata4vipday4.ipdb")
+ipdb = city:new("lua/ipdb/mydata4vipday4.ipdb")
+
+local ip = '123.125.114.144'
+local loc = ipdb:find(ip, "EN")
+
+cjson = require("cjson")
+local json = {}
+json["ret"] = "ok"
+json["data"] = loc
+ngx.say(loc['country_name'])
+ngx.say(loc['country_code'])
+ngx.say(loc['region_name'])
+ngx.say(loc['city_name'])
+ngx.say(loc['idd_code'])
+ngx.say(cjson.encode(json))
