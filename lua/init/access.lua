cdf5ed1b66e1d9af9939b166ba7535e9944c6f77
YiNo <dean@airdb.com>
2019-07-04 20:56:02 +0800

    update nginx.conf

--- lua/init/access.lua
+++ lua/init/access.lua
@@ -6,20 +6,20 @@ ngx.header['x-airdb-request-id'] = uustr.randomStr(64)
 --
 ngx.say(ngx.var.REMOTE_ADDR)
 
-local city = require("lua.ipdb.city")
+-- local city = require("lua.ipdb.city")
 -- ipdb = city:new("/usr/local/openresty/nginx/lua/ipdb/mydata4vipday4.ipdb")
-local ipdb = city:new("lua/ipdb/mydata4vipday4.ipdb")
-
-local ip = '123.125.114.144'
-local loc = ipdb:find(ip, "EN")
-
-local cjson = require("cjson")
-local json = {}
-json["ret"] = "ok"
-json["data"] = loc
-ngx.say(loc['country_name'])
-ngx.say(loc['country_code'])
-ngx.say(loc['region_name'])
-ngx.say(loc['city_name'])
-ngx.say(loc['idd_code'])
-ngx.say(cjson.encode(json))
+-- local ipdb = city:new("lua/ipdb/mydata4vipday4.ipdb")
+--
+-- local ip = '123.125.114.144'
+-- local loc = ipdb:find(ip, "EN")
+--
+-- local cjson = require("cjson")
+-- local json = {}
+-- json["ret"] = "ok"
+-- json["data"] = loc
+-- ngx.say(loc['country_name'])
+-- ngx.say(loc['country_code'])
+-- ngx.say(loc['region_name'])
+-- ngx.say(loc['city_name'])
+-- ngx.say(loc['idd_code'])
+-- ngx.say(cjson.encode(json))
